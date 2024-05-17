52762d9733036dafc9d422fbf0beb6e3394365a5
Ross Strachan <8963519+rbstrachan@users.noreply.github.com>
2024-05-17 00:29:48 -0400

    update binary2D with future image import code

--- binary2D/sketch.js
+++ binary2D/sketch.js
@@ -5,13 +5,15 @@
   // "allow colors to be inverted",
   // "allow an image to be uploaded, then converted into binary",
   // "add a list of example strings and the codes they produce",
-  // "simplify canvas and resolution calculations"
+  // "simplify canvas and resolution calculations",
+  // "add title and nav bar",
+  // "give error messages on invalid input"
 // ];
 
 let binaryString, codeVersion;
 let border = 0; // start the 2D code rectangle placement offset from the top left corner of the canvas
 
-const textArea = document.getElementById('qrData');
+const textArea = document.getElementById('codeData');
 // const button = document.getElementById('uploadImage');
 
 function setup() {  
@@ -64,3 +66,52 @@ function setBorder(n = 1) {
   draw2DCode();
   return console.log("Border set to " + n + "px.\n" + (n ? "Visual error detection is not available while a border is present.\nUse `setBorder(false);` to remove the border" : "Border removed. Visual error detection is available."));
 }
+
+/* ***
+
+// todo = [
+  // "2D code reader library that automatically detects code size", (partially done, only reads qr codes)
+  // "add ability to upload image",
+  // "add checks -- minimum and maximum image size, etc.",
+  // "add button to copy result"
+// ];
+
+let qrImage, size, binaryString;
+const textArea = document.getElementById("codeData");
+
+function preload() {
+  qrImage = loadImage("bfp33.png");
+}
+
+function setup() {
+  size = getQRCodeSize(qrImage);
+
+  for (let y = 0; y < size; y++) {
+    binaryString = "";
+    for (let x = 0; x < size; x++) {
+      const pixelColor = qrImage.get(
+        (x * qrImage.width) / size + qrImage.width / size / 2,
+        (y * qrImage.height) / size + qrImage.height / size / 2
+      );
+      // blendMode(DIFFERENCE);
+      // fill(255);
+      // rect(x * qrImage.width / size + qrImage.width / size / 2, y * qrImage.height / size + qrImage.height / size / 2, 1, 1);
+      const isBlack = brightness(pixelColor) < 50;
+      binaryString += isBlack ? "1" : "0";
+    }
+    textArea.value += binaryString + "\n";
+  }
+}
+
+function getQRCodeSize() {
+  qrImage.loadPixels();
+  const qrCode = jsQR(qrImage.pixels, qrImage.width, qrImage.height);
+
+  if (qrCode) {
+    console.log(`data: ${qrCode.data}\n\nversion: ${qrCode.version}\nsize: ${17 + (qrCode.version * 4)}`);
+    return 17 + (qrCode.version * 4);
+  } else {
+    throw new Error("Could not read a QR code from the image. This function only works with QR codes. Check the image you uploaded is a valid QR code a try again.");
+  }
+}
+*/
